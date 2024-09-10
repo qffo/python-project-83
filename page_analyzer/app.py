a151c3803ff92b8f744b7465d19a7fa9f3f0582c
qffo <075viktor@mail.ru>
2024-09-10 09:31:46 +0300

    add load_dotenv

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
 
-# Это callable WSGI-приложение
-app = Flask(__name__)
+
+load_dotenv()
+app = Flask(__name__)  # Это callable WSGI-приложение
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
