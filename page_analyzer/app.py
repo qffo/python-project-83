94ed08e597ef575b4efd4fa1ebf4335225942f3c
qffo <075viktor@mail.ru>
2024-09-14 11:10:15 +0300

    add validator url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 from flask import (
     Flask,
     render_template,
-    # request,
-    # redirect,
-    # url_for,
+    request,
+    redirect,
+    url_for,
+    flash,
+    get_flashed_messages
 )
+from .validator import validate
 
 import psycopg2
 
@@ -19,7 +22,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 def get_all_urls():
     urls = []
-    sql = "SELECT id, name FROM urls;"
+    sql = "SELECT id, name FROM urls ORDER BY id DESC;"
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
         cursor.execute(sql)
@@ -49,16 +52,73 @@ def hello_world():
     return render_template('index.html')
 
 
-# @app.route('/')
-# def index():
-#     with psycopg2.connect(DATABASE_URL) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT * from urls;")
-#         db_version = cursor.fetchone()
-#     conn.close()
-#     return f"{db_version}"
+def add_new_url(url_name):
+    query = 'INSERT INTO urls (name) VALUES (%s);'
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, (url_name,))
+        conn.commit()
+
+
+def get_id_by_name(url_name):
+    sql = '''
+    SELECT id FROM urls
+    WHERE name = %s;
+    '''
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_name,))
+        record = cursor.fetchone()
+    url_id = record[0] if record else None
+    return url_id
+
+
+@app.post('/urls')
+def add_url():
+    url_name = request.form.get('url', '', type=str)
+    if not validate(url_name):
+        flash('Некорректный URL', 'danger')
+        messages = get_flashed_messages(with_categories=True)
+        return render_template(
+            'index.html',
+            messages=messages,
+            url_name=url_name
+        ), 422
+
+    add_new_url(url_name)
+    url_id = get_id_by_name(url_name)
+    return redirect(url_for('one_url', url_id=url_id), 302)
+
+
+def get_one_urls(url_id):
+    url_info = {}
+    sql = 'SELECT * FROM urls WHERE id = %s;'
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_id,))
+        record = cursor.fetchone()
+    if record:
+        url_info['id'] = record[0]
+        url_info['name'] = record[1]
+        url_info['created_at'] = record[2]
+    return url_info
+
+
+@app.route('/urls/<int:url_id>')
+def one_url(url_id):
+    url_info = get_one_urls(url_id)
+    messages = get_flashed_messages(with_categories=True)
+    return render_template(
+        'one_url.html',
+        messages=messages,
+        url=url_info,
+    )
 
 
 @app.get('/urls')
 def get_urls():
     return render_template('all_urls.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ page_analyzer/validator.py
@@ -0,0 +1,9 @@
+import validators
+
+
+# validators.url(url) - функция проверяет, является ли переданная строка действительным URL.
+# Если URL корректен, она возвращает True, в противном случае False.
+def validate(url):
+    if validators.url(url) and len(url) <= 255:
+        return True
+    return False
