283805cfdddc2f0540609ccc0d6d4b99352221ff
qffo <075viktor@mail.ru>
2024-09-23 21:35:06 +0300

    add config database unit

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,6 @@
-import os
 import requests
 from dotenv import load_dotenv
+from .config import SECRET_KEY  # type: ignore
 from flask import (
     Flask,
     render_template,
@@ -14,46 +14,20 @@ from .validator import validate
 from .normalize_ur import normalize_url
 from .parser_bs4 import get_h1, get_title, get_descr
 
-import psycopg2
+from .database import (
+    get_all_urls,
+    add_new_url,
+    get_id_by_name,
+    get_one_urls,
+    sql_check_url,
+    get_checks_by_id
+)
+
 
 load_dotenv()
-app = Flask(__name__)  # Это callable WSGI-приложение
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app = Flask(__name__)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-
-def data_format(created_at):
-    '''форматируем дату в нормальную дату
-    если дата проверки отсутствует то None
-    если дата проверки есть то переводим её в нормальую дату'''
-    if created_at is None:
-        return None
-    return created_at.date()
-
-
-def get_all_urls():
-    urls = []
-    sql = """SELECT distinct on (urls.id)
-    urls.id, urls.name, url_checks.created_at, url_checks.status_code
-    FROM urls left join url_checks
-    on urls.id = url_checks.url_id
-    ORDER BY id DESC;"""
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        records = cursor.fetchall()
-    if records:
-        for record in records:
-            urls.append(
-                {
-                    'id': record[0],
-                    'name': record[1],
-                    'created_at': data_format(record[2]),
-                    'status_code': record[3]
-                }
-            )
-    return urls
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.get('/urls')
@@ -73,27 +47,6 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-
-def add_new_url(url_name):
-    query = 'INSERT INTO urls (name) VALUES (%s);'
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query, (url_name,))
-        conn.commit()
-
-
-def get_id_by_name(url_name):
-    sql = '''
-    SELECT id FROM urls
-    WHERE name = %s;
-    '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_name,))
-        record = cursor.fetchone()
-    url_id = record[0] if record else None
-    return url_id
 
 
 @app.post('/urls')
@@ -121,47 +74,14 @@ def add_url():
     return redirect(url_for('one_url', url_id=url_id), 302)
 
 
-def get_one_urls(url_id):
-    url_info = {}
-    sql = 'SELECT * FROM urls WHERE id = %s;'
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id,))
-        record = cursor.fetchone()
-    if record:
-        url_info['id'] = record[0]
-        url_info['name'] = record[1]
-        url_info['created_at'] = data_format(record[2])
-    return url_info
-
-
-def sql_check_url(url_id, st_code, bs4_h1, bs4_title, bs4_descr):
-    sql = '''
-    INSERT INTO url_checks
-    (url_id, status_code, h1, title, description)
-    VALUES (%s, %s, %s, %s, %s);
-    '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id, st_code, bs4_h1, bs4_title, bs4_descr))
-        conn.commit()
-
-
 @app.post('/urls/<int:url_id>/checks')
 def check_url(url_id):
     url_info = get_one_urls(url_id)
 
     if not url_info:
         return render_template('404.html'), 404
-
-    # try - обработка возможных исключений, идёт вместе с except
-    # В этом блоке выполняется код, который может вызвать исключение.
     try:
         r = requests.get(url_info['name'])
-
-        # Метод resp.raise_for_status() проверяет, был ли запрос успешным
-        # Если код состояния не 200 (например, 404 или 500),
-        # будет вызвано исключение HTTPError
         r.raise_for_status()
         bs4_h1 = get_h1(url_info['name'])
         bs4_title = get_title(url_info['name'])
@@ -171,43 +91,9 @@ def check_url(url_id):
         sql_check_url(url_id, r.status_code, bs4_h1, bs4_title, bs4_descr)
         return redirect(url_for('one_url', url_id=url_id), 302)
 
-    # except - Этот блок отлавливает все исключения, связанные с запросами,
-    # и выполняет код внутри него, если произошло исключение
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
         return redirect(url_for('one_url', url_id=url_id))
-
-
-def get_checks_by_id(url_id):
-    url_checks = []
-    sql = '''
-    SELECT
-        url_checks.id,
-        url_checks.status_code,
-        url_checks.created_at,
-        url_checks.h1,
-        url_checks.title,
-        url_checks.description
-    FROM url_checks
-    WHERE url_checks.url_id = %s
-    ORDER BY url_checks.id DESC;
-    '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id,))
-        records = cursor.fetchall()
-    if records:
-        for record in records:
-            url_checks.append(
-                {'id': record[0],
-                 'status_code': record[1],
-                 'created_at': data_format(record[2]),
-                 'h1': record[3],
-                 'title': record[4],
-                 'description': record[5]
-                 }
-            )
-    return url_checks
 
 
 @app.route('/urls/<int:url_id>')
---
+++ page_analyzer/database.py
@@ -0,0 +1,107 @@
+import psycopg2
+from .config import DATABASE_URL  # type: ignore
+
+from .utils import normilize_date
+
+
+def get_all_urls():
+    urls = []
+    sql = """SELECT distinct on (urls.id)
+    urls.id, urls.name, url_checks.created_at, url_checks.status_code
+    FROM urls left join url_checks
+    on urls.id = url_checks.url_id
+    ORDER BY id DESC;"""
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        records = cursor.fetchall()
+    if records:
+        for record in records:
+            urls.append(
+                {
+                    'id': record[0],
+                    'name': record[1],
+                    'created_at': normilize_date(record[2]),
+                    'status_code': record[3]
+                }
+            )
+    return urls
+
+
+def add_new_url(url_name):
+    query = 'INSERT INTO urls (name) VALUES (%s);'
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, (url_name,))
+        conn.commit()
+
+
+def get_id_by_name(url_name):
+    sql = '''
+    SELECT id FROM urls
+    WHERE name = %s;
+    '''
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_name,))
+        record = cursor.fetchone()
+    url_id = record[0] if record else None
+    return url_id
+
+
+def get_one_urls(url_id):
+    url_info = {}
+    sql = 'SELECT * FROM urls WHERE id = %s;'
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_id,))
+        record = cursor.fetchone()
+    if record:
+        url_info['id'] = record[0]
+        url_info['name'] = record[1]
+        url_info['created_at'] = normilize_date(record[2])
+    return url_info
+
+
+def sql_check_url(url_id, st_code, bs4_h1, bs4_title, bs4_descr):
+    sql = '''
+    INSERT INTO url_checks
+    (url_id, status_code, h1, title, description)
+    VALUES (%s, %s, %s, %s, %s);
+    '''
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_id, st_code, bs4_h1, bs4_title, bs4_descr))
+        conn.commit()
+
+
+def get_checks_by_id(url_id):
+    url_checks = []
+    sql = '''
+    SELECT
+        url_checks.id,
+        url_checks.status_code,
+        url_checks.created_at,
+        url_checks.h1,
+        url_checks.title,
+        url_checks.description
+    FROM url_checks
+    WHERE url_checks.url_id = %s
+    ORDER BY url_checks.id DESC;
+    '''
+    with psycopg2.connect(DATABASE_URL) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_id,))
+        records = cursor.fetchall()
+    if records:
+        for record in records:
+            url_checks.append(
+                {'id': record[0],
+                 'status_code': record[1],
+                 'created_at': normilize_date(record[2]),
+                 'h1': record[3],
+                 'title': record[4],
+                 'description': record[5]
+                 }
+            )
+    return url_checks
