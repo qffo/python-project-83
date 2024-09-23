3f849565b823ea3d48c4181f56ee0f78487bac29
qffo <075viktor@mail.ru>
2024-09-23 22:00:00 +0300

    norm url doc string

--- page_analyzer/normalize_ur.py
+++ page_analyzer/normalize_ur.py
@@ -2,12 +2,10 @@ from urllib.parse import urlparse, urlunparse
 
 
 def normalize_url(url):
-    '''Функция использует urlunparse для создания нового URL.
-    В новом URL сохраняется:
-    parsed_url.scheme: схема URL (например, http, https).
-    parsed_url.hostname: хост (доменное имя) URL.
-    Пустые строки для остальных компонентов (path, params, query, fragment).
-    Например, если URL https://www.example.com/some/path?query=param#fragment,
-    функция вернет https://www.example.com.'''
+    '''Returns a normalized URL, preserving only the scheme and host.
+    Example:
+        normalize_url("https://www.example.com/some/path?query=param#fragment")
+        will return "https://www.example.com".
+    '''
     parsed_url = urlparse(url)
     return urlunparse((parsed_url.scheme, parsed_url.hostname, '', '', '', ''))
