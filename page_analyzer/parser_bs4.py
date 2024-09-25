680edfd299899b9df8a1d6d7b798b3d84773bbe6
qffo <075viktor@mail.ru>
2024-09-25 21:20:47 +0300

    black box #10

--- page_analyzer/parser_bs4.py
+++ page_analyzer/parser_bs4.py
@@ -3,48 +3,49 @@ from bs4 import BeautifulSoup
 
 
 def get_h1(url):
-    # Получение HTML-кода страницы
     response = requests.get(url)
-
-    # Проверка успешности запроса
     if response.status_code == 200:
-        # Создание объекта BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Поиск тега <h1>
-        h1_tag = soup.find('h1')
-
-        # Проверка наличия тега <h1> и вывод его содержимого
-        if h1_tag:
-            return h1_tag.text
-        else:
-            return ''
+        return get_h1_from_html(response.text)
     else:
-        return f'Ошибка при получении страницы: {response.status_code}'
+        raise Exception(
+            f'Ошибка при получении страницы: {response.status_code}')
+
+
+def get_h1_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    h1_tag = soup.find('h1')
+    return h1_tag.text if h1_tag else ''
 
 
 def get_title(url):
     response = requests.get(url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        title_tag = soup.find('title')
-        if title_tag:
-            return title_tag.text
-        else:
-            return ''
+        return get_title_from_html(response.text)
     else:
-        return f'Ошибка при получении страницы: {response.status_code}'
+        raise Exception(
+            f'Ошибка при получении страницы: {response.status_code}')
+
+
+def get_title_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    title_tag = soup.find('title')
+    return title_tag.text if title_tag else ''
 
 
 def get_descr(url):
     response = requests.get(url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        descr_tag = soup.find('meta', attrs={'name': 'description'})
-        if descr_tag and 'content' in descr_tag.attrs:
-            content_value = descr_tag['content']
-            return content_value
-        else:
-            return ''
+        return get_descr_from_html(response.text)
     else:
-        return f'Ошибка при получении страницы: {response.status_code}'
+        raise Exception(
+            f'Ошибка при получении страницы: {response.status_code}')
+
+
+def get_descr_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    descr_tag = soup.find('meta', attrs={'name': 'description'})
+    if descr_tag and 'content' in descr_tag.attrs:
+        content_value = descr_tag['content']
+        return content_value
+    else:
+        return ''
