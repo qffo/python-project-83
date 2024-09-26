6783f720f12b66df787f9b3c47bf99f01ea97f36
qffo <075viktor@mail.ru>
2024-09-26 20:52:25 +0300

    add connection_pool #5

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -1,5 +1,9 @@
 import psycopg2
+from psycopg2 import pool
 from .config import DATABASE_URL  # type: ignore
+
+
+connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
 
 
 def get_all_urls():
@@ -9,31 +13,41 @@ def get_all_urls():
     FROM urls left join url_checks
     on urls.id = url_checks.url_id
     ORDER BY id DESC;"""
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        records = cursor.fetchall()
-    if records:
-        for record in records:
-            urls.append(
-                {
-                    'id': record[0],
-                    'name': record[1],
-                    'created_at': (
-                        record[2].date() if record[2] is not None else None
-                    ),
-                    'status_code': record[3]
-                }
-            )
+
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            records = cursor.fetchall()
+        if records:
+            for record in records:
+                urls.append(
+                    {
+                        'id': record[0],
+                        'name': record[1],
+                        'created_at': (
+                            record[2].date() if record[2] is not None else None
+                        ),
+                        'status_code': record[3]
+                    }
+                )
+    finally:
+        connection_pool.putconn(conn)
     return urls
 
 
 def add_new_url(url_name):
-    query = 'INSERT INTO urls (name) VALUES (%s);'
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query, (url_name,))
-        conn.commit()
+    sql = 'INSERT INTO urls (name) VALUES (%s);'
+
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, (url_name,))
+            conn.commit()
+    finally:
+        connection_pool.putconn(conn)
 
 
 def get_id_by_name(url_name):
@@ -41,27 +55,37 @@ def get_id_by_name(url_name):
     SELECT id FROM urls
     WHERE name = %s;
     '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_name,))
-        record = cursor.fetchone()
-    url_id = record[0] if record else None
+
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, (url_name,))
+            record = cursor.fetchone()
+        url_id = record[0] if record else None
+    finally:
+        connection_pool.putconn(conn)
     return url_id
 
 
 def get_one_urls(url_id):
     url_info = {}
     sql = 'SELECT * FROM urls WHERE id = %s;'
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id,))
-        record = cursor.fetchone()
-    if record:
-        url_info['id'] = record[0]
-        url_info['name'] = record[1]
-        url_info['created_at'] = (
-            record[2].date() if record[2] is not None else None
-        )
+
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, (url_id,))
+            record = cursor.fetchone()
+        if record:
+            url_info['id'] = record[0]
+            url_info['name'] = record[1]
+            url_info['created_at'] = (
+                record[2].date() if record[2] is not None else None
+            )
+    finally:
+        connection_pool.putconn(conn)
     return url_info
 
 
@@ -71,10 +95,14 @@ def sql_check_url(url_id, st_code, bs4_h1, bs4_title, bs4_descr):
     (url_id, status_code, h1, title, description)
     VALUES (%s, %s, %s, %s, %s);
     '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id, st_code, bs4_h1, bs4_title, bs4_descr))
-        conn.commit()
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, (url_id, st_code, bs4_h1, bs4_title, bs4_descr))
+            conn.commit()
+    finally:
+        connection_pool.putconn(conn)
 
 
 def get_checks_by_id(url_id):
@@ -91,22 +119,26 @@ def get_checks_by_id(url_id):
     WHERE url_checks.url_id = %s
     ORDER BY url_checks.id DESC;
     '''
-    with psycopg2.connect(DATABASE_URL) as conn:
-        cursor = conn.cursor()
-        cursor.execute(sql, (url_id,))
-        records = cursor.fetchall()
-    if records:
-        for record in records:
-            url_checks.append(
-                {
-                    'id': record[0],
-                    'status_code': record[1],
-                    'created_at': (
-                        record[2].date() if record[2] is not None else None
-                    ),
-                    'h1': record[3],
-                    'title': record[4],
-                    'description': record[5]
-                }
-            )
+    conn = connection_pool.getconn()
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, (url_id,))
+            records = cursor.fetchall()
+        if records:
+            for record in records:
+                url_checks.append(
+                    {
+                        'id': record[0],
+                        'status_code': record[1],
+                        'created_at': (
+                            record[2].date() if record[2] is not None else None
+                        ),
+                        'h1': record[3],
+                        'title': record[4],
+                        'description': record[5]
+                    }
+                )
+    finally:
+        connection_pool.putconn(conn)
     return url_checks
