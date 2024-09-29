3794345f5e3db8f2416c74d3f4d12a804445efc9
qffo <075viktor@mail.ru>
2024-09-29 19:09:50 +0300

    docs: add docstrings to functions
    This commit adds docstrings to the get_h1, get_title, and get_descr functions

--- page_analyzer/parser_bs4.py
+++ page_analyzer/parser_bs4.py
@@ -2,18 +2,27 @@ from bs4 import BeautifulSoup
 
 
 def get_h1(response: str) -> str:
+    """
+    Extract the <h1> tag content from the given HTML response.
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     h1_tag = soup.find('h1')
     return h1_tag.text if h1_tag else None
 
 
 def get_title(response: str) -> str:
+    """
+    Extract the <title> tag content from the given HTML response.
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     title_tag = soup.find('title')
     return title_tag.text if title_tag else None
 
 
 def get_descr(response: str) -> str:
+    """
+    Extract the content of the <meta name="description"> tag from the given HTML response.
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     descr_tag = soup.find('meta', attrs={'name': 'description'})
     if descr_tag and 'content' in descr_tag.attrs:
--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,6 +1,5 @@
 import validators
 import logging
-import psycopg2
 import requests
 from .parser_bs4 import get_h1, get_title, get_descr
 from .database import (
@@ -19,7 +18,7 @@ def validate(url: str) -> bool:
     return validators.url(url) and len(url) <= 255
 
 
-def perform_url_check(url_id):  # noqa: C901
+def perform_url_check(url_id):
     """
     Check the status of a URL and save the results to the database.
     """
@@ -31,18 +30,15 @@ def perform_url_check(url_id):  # noqa: C901
     try:
         response = requests.get(url_info['name'])
         response.raise_for_status()
-        bs4_h1 = get_h1(response)
-        bs4_title = get_title(response)
-        bs4_descr = get_descr(response)
+
     except Exception:
         return 'Произошла ошибка при проверке', 500, 'danger'
 
-    try:
-        sql_check_url(url_id, response.status_code,
-                      bs4_h1, bs4_title, bs4_descr)
-    except psycopg2.DatabaseError:
-        return 'Ошибка базы данных при сохранении данных', 500, 'danger'
-    except Exception as e:
-        return f'Ошибка при сохранении данных: {str(e)}', 500, 'danger'
+    bs4_h1 = get_h1(response)
+    bs4_title = get_title(response)
+    bs4_descr = get_descr(response)
+
+    sql_check_url(url_id, response.status_code,
+                  bs4_h1, bs4_title, bs4_descr)
 
     return 'Страница успешно проверена', 200, 'success'
