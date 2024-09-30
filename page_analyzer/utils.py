d0c85ae2e3865eb5cc99eeb01d5e9a93d8bd41ab
qffo <075viktor@mail.ru>
2024-09-30 23:00:18 +0300

    refactor(utils): added logging for errors and split the perform_url_check function into four get_url_info_or_404 , fetch_url , parse_response , save_to_database

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import psycopg2
 import validators
 import logging
 import requests
@@ -7,7 +9,13 @@ from .database import (
     sql_check_url,
 )
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(
+    level=logging.ERROR,
+    filename="log.log",
+    format="%(asctime)s %(levelname)s %(message)s",
+    # filemode='w'
+)
+
 logger = logging.getLogger(__name__)
 
 
@@ -20,25 +28,101 @@ def validate(url: str) -> bool:
 
 def perform_url_check(url_id):
     """
-    Check the status of a URL and save the results to the database.
+    Main function to check the status of a URL and save the results.
     """
-    url_info = get_one_url(url_id)
+    url_info, error_response = get_url_info_or_404(url_id)
+    if error_response:
+        return error_response
 
-    if not url_info:
-        return None, 404
+    response, error_response = fetch_url(url_info['name'])
+    if error_response:
+        return error_response
 
-    try:
-        response = requests.get(url_info['name'])
-        response.raise_for_status()
+    bs4_h1, bs4_title, bs4_descr = parse_response(response, url_info['name'])
 
-    except Exception:
-        return 'Произошла ошибка при проверке', 500, 'danger'
-
-    bs4_h1 = get_h1(response)
-    bs4_title = get_title(response)
-    bs4_descr = get_descr(response)
-
-    sql_check_url(url_id, response.status_code,
-                  bs4_h1, bs4_title, bs4_descr)
+    save_to_database(url_id, response.status_code, bs4_h1, bs4_title, bs4_descr)
 
     return 'Страница успешно проверена', 200, 'success'
+
+
+def get_url_info_or_404(url_id):
+    """
+    Get URL info or return a 404 error if not found.
+    """
+    url_info = get_one_url(url_id)
+    if not url_info:
+        return None, (None, 404)
+    return url_info, None
+
+
+def fetch_url(url):
+    """
+    Fetch the URL and handle request errors.
+    """
+    default_error_msg = 'Произошла ошибка при проверке'
+
+    class ErrorMessage(str, Enum):
+        httperror = 'Ошибка HTTP при проверке URL'
+        connecterror = 'Ошибка соединения при проверке URL'
+        timeout = 'Тайм-аут при проверке URL'
+        requestsexc = 'Исключение из запроса при проверке URL'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response, None
+
+    except requests.exceptions.HTTPError:
+        logger.error(ErrorMessage.httperror, exc_info=True)
+        return None, (default_error_msg, 500, 'danger')
+
+    except requests.exceptions.ConnectionError:
+        logger.error(ErrorMessage.connecterror, extra=response)
+        return None, (default_error_msg, 500, 'danger')
+
+    except requests.exceptions.Timeout:
+        logger.error(ErrorMessage.timeout, extra=response)
+        return None, (default_error_msg, 500, 'danger')
+
+    except requests.exceptions.RequestException:
+        logger.error(ErrorMessage.requestsexc, extra=response)
+        return None, (default_error_msg, 500, 'danger')
+
+
+def parse_response(response, url):
+    """
+    Parse the response to extract h1, title, and description.
+    """
+    try:
+        bs4_h1 = get_h1(response)
+        bs4_title = get_title(response)
+        bs4_descr = get_descr(response)
+        return bs4_h1, bs4_title, bs4_descr
+
+    except (AttributeError, ValueError) as e:
+        logger.error(
+            f"{type(e).__name__} при разборе URL-адреса {url}", exc_info=True)
+
+    except Exception as e:
+        logger.error(
+            f"Ошибка при анализе URL-адреса {url}: {str(e)}",
+            exc_info=True)
+
+    return None, None, None
+
+
+def save_to_database(url_id, status_code, h1, title, descr):
+    """
+    Save the check result into the database.
+    """
+    try:
+        sql_check_url(url_id, status_code, h1, title, descr)
+    except psycopg2.DatabaseError:
+        logger.error(
+            f"Ошибка БД при сохранении URL-адреса с id {url_id}",
+            exc_info=True)
+    except Exception as e:
+        logger.error(
+            f"Ошибка при сохранении URL-адреса с id {url_id}: {str(e)}",
+            exc_info=True
+        )
