71aa48ce8d4ec42f068e903a230781e0c3c0dc7d
qffo <075viktor@mail.ru>
2024-10-01 10:51:14 +0300

    refactor(database): add context manager
    Implemented the logic through the context manager when issuing a connection from the pool

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -1,10 +1,20 @@
+from contextlib import contextmanager
 from psycopg2 import pool
 from .config import DATABASE_URL  # type: ignore
 
 connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
 
 
-def get_all_urls(conn=None):
+@contextmanager
+def get_connection():
+    conn = connection_pool.getconn()
+    try:
+        yield conn
+    finally:
+        connection_pool.putconn(conn)
+
+
+def get_all_urls():
     urls = []
     sql = """SELECT distinct on (urls.id)
     urls.id, urls.name, url_checks.created_at, url_checks.status_code
@@ -12,8 +22,7 @@ def get_all_urls(conn=None):
     on urls.id = url_checks.url_id
     ORDER BY id DESC;"""
 
-    conn = conn or connection_pool.getconn()
-    try:
+    with get_connection() as conn:
         with conn:
             cursor = conn.cursor()
             cursor.execute(sql)
@@ -30,48 +39,39 @@ def get_all_urls(conn=None):
                         'status_code': record[3]
                     }
                 )
-    finally:
-        connection_pool.putconn(conn)
     return urls
 
 
-def add_new_url(url_name, conn=None):
+def add_new_url(url_name):
     sql = 'INSERT INTO urls (name) VALUES (%s);'
 
-    conn = conn or connection_pool.getconn()
-    try:
+    with get_connection() as conn:
         with conn:
             cursor = conn.cursor()
             cursor.execute(sql, (url_name,))
             conn.commit()
-    finally:
-        connection_pool.putconn(conn)
 
 
-def get_id_by_name(url_name, conn=None):
+def get_id_by_name(url_name):
     sql = '''
     SELECT id FROM urls
     WHERE name = %s;
     '''
 
-    conn = conn or connection_pool.getconn()
-    try:
+    with get_connection() as conn:
         with conn:
             cursor = conn.cursor()
             cursor.execute(sql, (url_name,))
             record = cursor.fetchone()
         url_id = record[0] if record else None
-    finally:
-        connection_pool.putconn(conn)
     return url_id
 
 
-def get_one_url(url_id, conn=None):
+def get_one_url(url_id):
     url_info = {}
     sql = 'SELECT * FROM urls WHERE id = %s;'
 
-    conn = conn or connection_pool.getconn()
-    try:
+    with get_connection() as conn:
         with conn:
             cursor = conn.cursor()
             cursor.execute(sql, (url_id,))
@@ -80,25 +80,20 @@ def get_one_url(url_id, conn=None):
             url_info['id'] = record[0]
             url_info['name'] = record[1]
             url_info['created_at'] = record[2].date()
-    finally:
-        connection_pool.putconn(conn)
     return url_info
 
 
-def sql_check_url(url_id, st_code, bs4_h1, bs4_title, bs4_descr, conn=None):
+def sql_check_url(url_id, st_code, bs4_h1, bs4_title, bs4_descr):
     sql = '''
     INSERT INTO url_checks
     (url_id, status_code, h1, title, description)
     VALUES (%s, %s, %s, %s, %s);
     '''
-    conn = conn or connection_pool.getconn()
-    try:
+    with get_connection() as conn:
         with conn:
             cursor = conn.cursor()
             cursor.execute(sql, (url_id, st_code, bs4_h1, bs4_title, bs4_descr))
             conn.commit()
-    finally:
-        connection_pool.putconn(conn)
 
 
 def get_checks_by_id(url_id, conn=None):
@@ -115,24 +110,20 @@ def get_checks_by_id(url_id, conn=None):
     WHERE url_checks.url_id = %s
     ORDER BY url_checks.id DESC;
     '''
-    conn = conn or connection_pool.getconn()
-    try:
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute(sql, (url_id,))
-            records = cursor.fetchall()
-        if records:
-            for record in records:
-                url_checks.append(
-                    {
-                        'id': record[0],
-                        'status_code': record[1],
-                        'created_at': record[2].date(),
-                        'h1': record[3],
-                        'title': record[4],
-                        'description': record[5]
-                    }
-                )
-    finally:
-        connection_pool.putconn(conn)
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, (url_id,))
+        records = cursor.fetchall()
+    if records:
+        for record in records:
+            url_checks.append(
+                {
+                    'id': record[0],
+                    'status_code': record[1],
+                    'created_at': record[2].date(),
+                    'h1': record[3],
+                    'title': record[4],
+                    'description': record[5]
+                }
+            )
     return url_checks
