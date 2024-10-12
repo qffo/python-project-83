a42dd7efb8781e39f661af5efded547d03363a9b
qffo <075viktor@mail.ru>
2024-10-12 19:41:18 +0300

    refactor(utils): added custom "Page Not Found" and "Page Check Error"
    exceptions to control the logic at the application level.
    "perform_curl_check" now checks for the URL and delegates the fetch_url call, while throwing the appropriate exception if it fails.

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -22,17 +22,40 @@ def validate(url: str) -> bool:
     return validators.url(url) and len(url) <= 255
 
 
-def perform_url_check(url_id):
-    """
-    Main function to check the status of a URL and save the results.
-    """
-    url_info, error_response = get_url_info_or_404(url_id)
-    if error_response:
-        return error_response
+class PageAnalyzerError(Exception):
+    pass
 
-    response, error_response = fetch_url(url_info['name'])
-    if error_response:
-        return error_response
+
+class PageNotFound(PageAnalyzerError):
+    pass
+
+
+class PageCheckError(PageAnalyzerError):
+    pass
+
+
+def get_url_info(url_id):
+    """
+    Get URL info or return None if not found.
+    """
+    url_info = get_one_url(url_id)
+    if not url_info:
+        return None
+    return url_info
+
+
+def perform_url_check(url_id) -> str:
+    """
+    Main function to check the status of a URL and handle the results.
+    Raises custom exceptions in case of errors.
+    """
+    url_info = get_url_info(url_id)
+    if not url_info:
+        raise PageNotFound('URL not found')
+
+    response = fetch_url(url_info['name'])
+    if response is None:
+        raise PageCheckError('Error checking the URL')
 
     bs4_h1, bs4_title, bs4_descr = parse_response(response, url_info['name'])
 
@@ -41,48 +64,30 @@ def perform_url_check(url_id):
     return 'Страница успешно проверена', 200, 'success'
 
 
-def get_url_info_or_404(url_id):
-    """
-    Get URL info or return a 404 error if not found.
-    """
-    url_info = get_one_url(url_id)
-    if not url_info:
-        return None, (None, 404)
-    return url_info, None
+class ErrorMessage(str, Enum):
+    httperror = 'HTTP error when checking the URL'
+    connecterror = 'Connection error when checking the URL'
+    timeout = 'Timeout when checking the URL'
+    requestsexc = 'Request exception when checking the URL'
 
 
 def fetch_url(url):
     """
     Fetch the URL and handle request errors.
     """
-    default_error_msg = 'Произошла ошибка при проверке'
-
-    class ErrorMessage(str, Enum):
-        httperror = 'HTTP error when checking the URL'
-        connecterror = 'Connection error when checking the URL'
-        timeout = 'Timeout when checking the URL'
-        requestsexc = 'Exclusion from the request when checking the URL'
-
     try:
         response = requests.get(url)
         response.raise_for_status()
-        return response, None
-
+        return response
     except requests.exceptions.HTTPError:
         logger.error(ErrorMessage.httperror, exc_info=True)
-        return None, (default_error_msg, 500, 'danger')
-
     except requests.exceptions.ConnectionError:
         logger.error(ErrorMessage.connecterror, exc_info=True)
-        return None, (default_error_msg, 500, 'danger')
-
     except requests.exceptions.Timeout:
         logger.error(ErrorMessage.timeout, extra=response)
-        return None, (default_error_msg, 500, 'danger')
-
     except requests.exceptions.RequestException:
         logger.error(ErrorMessage.requestsexc, exc_info=True)
-        return None, (default_error_msg, 500, 'danger')
+    return None
 
 
 class ParseError(Exception):
